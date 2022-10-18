24ade0cb1208c878c6e3e2cb11638be43f6297d3
elizabeth chernysheva <lizzychernysheva@gmail.com>
2022-10-18 19:12:30 +0300

    add local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,14 +8,26 @@ class App extends Component {
 
   state = {
     contacts: [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+      {id: nanoid(), name: 'Rosie Simpson', number: '459-12-56'},
+      {id: nanoid(), name: 'Hermione Kline', number: '443-89-12'},
     ],
     filter: '',
   }
-  //
+
+  componentDidMount() {
+    const contacts = JSON.parse(localStorage.getItem('contacts'));
+    if (contacts) {
+      this.setState({
+        contacts,
+      });
+    }
+  }
+  componentDidUpdate(prevProps, presvState) {
+    if (presvState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   handleOnSubmit = ({ name,number }) => {
 
     if(this.state.contacts.some(el => el.name === name)){
@@ -59,4 +71,4 @@ class App extends Component {
 };
 
 
-export default App
+export default App;
